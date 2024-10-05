037fa0c5e7a72b43644766ca7de36b3b6b2a6ae9
Vinicius <viniipvp@gmail.com>
2024-10-05 10:05:28 -0300

    ok

--- tst.py
+++ tst.py
@@ -1,1 +1,9 @@
-print ("helloworldz")
+import seaborn as sns
+import pandas as pd
+
+pinguins = sns.load_dataset('penguins')
+
+print(pinguins.head(4))
+print("--------------")
+pinguins = pinguins.dropna()
+print(pinguins.head(4))
